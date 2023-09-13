969169a170af9f952c44bda6c16f1102cb8d7403
ketan sarna <ketansarna@icloud.com>
2023-09-13 17:42:48 +0530

    header created

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,56 @@
-import "./Header.scss";
-
-import {useState,useEffect,useContext} from 'react'
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-//icons
-import {TbSearch} from "react-icons/tb";
-import {CgShoppingCart} from "react-icons/cg"
-import {AiOutLineHeart} from "react-icons/ai"
-//conponents
-import Search from "./Search/Search"
-import Cart from "../Cart/Cart"
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+import "./Header.scss";
+import Search from "./Search/Search";
 import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
 
 const Header = () => {
-    return <div>Header</div>;
+    const [scrolled, setScrolled] = useState(false);
+    const navigate = useNavigate();
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+
+    return (
+        <>
+            <header
+                className={`main-header ${scrolled ? "sticky-header" : ""}`}
+            >
+                <div className="header-content">
+                    <ul className="left">
+                        <li onClick={() => navigate("/")}>Home</li>
+                        <li onClick={() => navigate("/about")}>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center" onClick={() => navigate("/")}>
+                        JSDEVSTORE.
+                    </div>
+                    <div className="right">
+                        <AiOutlineHeart />
+                        <span
+                            className="cart-icon"
+                        >
+                         
+                        </span>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Header;
